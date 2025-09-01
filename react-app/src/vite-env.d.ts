b71ec588b82ff14a8724dbce21a8d67c5745e42d
Takumi0402 <itakumi8823@gmail.com>
2025-09-02 00:06:59 +0900

    Updates

--- react-app/src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-import type { PyodideInterface as PyodideInterfaceFromPackage } from "pyodide";
-
-declare global {
-  interface Window {
-    loadPyodide: (config?: { indexURL: string }) => Promise<PyodideInterfaceFromPackage>;
-  }
-}
